c196a61f0f53bd1cd2d891d3a1c17c550ea6d463
jensonjenkins <dave.martheen@gmail.com>
2024-10-05 14:02:54 +0800

    Add std algorithms to iterator_tests

--- tests/iterator_test.cpp
+++ tests/iterator_test.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <vector>
 #include "../src/array.hpp"
 
@@ -39,12 +41,22 @@ int main(int argc, char* argv[]){
     static_assert(std::is_same_v<decltype(b.data().data()), int*>);
     static_assert(std::is_same_v<decltype(b)::element_dim_type, ndim::static_dim<ndim::unit_dim, 10>>);
 
-    for(auto x : b){
+    for(auto x : b) {
         static_assert(std::is_same_v<decltype(x), ndim::array_ref<int, 10>>);
-        for(auto& y : x){
+        for(auto& y : x) {
             static_assert(std::is_same_v<decltype(y), int&>);
         }
     }
+
+    for(auto x = b.begin(); x != b.end(); x++) {
+        static_assert(std::is_same_v<decltype(x), 
+                ndim::iterator_intermediate_impl<ndim::inner_array<int, 10>, false>>);
+        static_assert(std::is_same_v<decltype(*x), ndim::array_ref<int, 10>>);
+        static_assert(std::is_same_v<decltype(x->begin()), ndim::iterator_lowest_impl<int, false>>);
+        for(auto y = x->begin(); y != x->end(); y++) {}
+    }
+
+    std::cout<<"2 - ok: 2 dimension ndim::array"<<std::endl;
 
     for(int i=0;i<3;i++){
         for(int j=0;j<10;j++){
@@ -52,20 +64,34 @@ int main(int argc, char* argv[]){
         }
     }
 
-    for(auto x = b.begin(); x != b.end(); x++){
-        static_assert(std::is_same_v<decltype(x), 
-                ndim::iterator_intermediate_impl<ndim::inner_array<int, 10>, false>>);
-        static_assert(std::is_same_v<decltype(*x), ndim::array_ref<int, 10>>);
-        static_assert(std::is_same_v<decltype(x->begin()), ndim::iterator_lowest_impl<int, false>>);
-        std::sort(x->begin(), x->end());
+    for(auto x : b){
+        std::sort(x.begin(), x.end());
         int inc = 1;
-        for(auto y = x->begin(); y != x->end(); y++){
-            assert(inc == *y);
+        for(auto y : x){
+            assert(inc == y);
             inc++;
         }
     }
+    std::cout<<"3 - ok: std::sort"<<std::endl;
 
-    std::cout<<"2 - ok: 2 dimension ndim::array"<<std::endl;
+    int ub = std::upper_bound(b[0].begin(), b[0].end(), 5) - b[0].begin();
+    int lb = std::lower_bound(b[0].begin(), b[0].end(), 5) - b[0].begin();
+    assert(ub == 5 && b[0][ub] == 6);
+    assert(lb == 4 && b[0][lb] == 5);
+
+    std::cout<<"4 - ok: std::lower_bound, std::upper_bound"<<std::endl;
+
+    int mx = *std::max_element(b[0].begin(), b[0].end());
+    int mn = *std::min_element(b[0].begin(), b[0].end());
+    assert(mx == 10);
+    assert(mn == 1);
+
+    std::cout<<"5 - ok: std::max_element, std::min_element"<<std::endl;
+
+    int sum = std::accumulate(b[0].begin(), b[0].end(), 0);
+    assert(sum == 55);
+
+    std::cout<<"6 - ok: std::accumulate"<<std::endl;
 
     exit(0);
 }
